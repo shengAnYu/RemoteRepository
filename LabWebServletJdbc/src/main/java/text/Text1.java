1eb8e31a513fabefedc713560247778c0af35089
于勝安 <Student@III-ITC>
2017-03-24 17:03:14 +0800

    第3次 commit
    將應用程式.Java Resources新增了Text.java
    列印三次Hellow word

--- LabWebServletJdbc/src/main/java/text/Text1.java
+++ LabWebServletJdbc/src/main/java/text/Text1.java
@@ -4,6 +4,8 @@ public class Text1 {
 
 	public static void main(String[] args) {
 		System.out.println("Hwllo word");
+		System.out.println("Hwllo word");
+		System.out.println("Hwllo word");
 
 	}
 
